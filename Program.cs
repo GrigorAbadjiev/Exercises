c8ab08f05b38800771e3ad09d24227dda479984c
User <User@DESKTOP-ESP2JT1>
2022-11-10 15:31:41 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // read n       
+            int n = int.Parse(Console.ReadLine());
+            double sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                sum += num;
+            }
+
+            double average = sum / n;
+            Console.WriteLine($"{average:f2}");
+            // sum
+            // average sum / n
+        }
+        // input // output
+        // 4            
+        // 95         54.25
+        // 23
+        // 76
+        // 23
+    }
+}
